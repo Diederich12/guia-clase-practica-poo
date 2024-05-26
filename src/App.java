b3fec672fb102e90a6de19cad10f2c10834c3acc
Diederich <diederich.gonzalez447@std.uni.edu.ni>
2024-05-26 09:21:09 -0600

    guia-clase-practica-poo

---
+++ src/App.java
@@ -0,0 +1,15 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        Auto auto = new Auto("Toyota", "Corolla", 2020, 15000, 5, "Gasolina");
+
+        Camion Camion = new Camion("Volvo", "Fh", 2019, 75000, 2000, 4);
+
+        // Mostrar la informacion de cada vehiculo
+        System.out.println("\nInformacion del Auto: ");
+        auto.mostrarInfo();
+
+        System.out.println("\nInformacion del Camion: ");
+        Camion.mostrarInfo();
+    }
+}
